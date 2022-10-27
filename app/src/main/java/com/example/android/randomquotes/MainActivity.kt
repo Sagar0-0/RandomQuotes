b9cbf790cb8fc490af0905e2652360572340e64b
Pulkit <pulkit161001@gmail.com>
2022-10-27 21:39:48 +0530

    Design Changed

--- app/src/main/java/com/example/android/randomquotes/MainActivity.kt
+++ app/src/main/java/com/example/android/randomquotes/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.example.android.randomquotes
 
-import android.app.Application
-import android.content.ContentValues.TAG
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.android.randomquotes.api.QuoteService
-import com.example.android.randomquotes.api.RetrofitHelper
-import com.example.android.randomquotes.repo.QuotesRepo
 import com.example.android.randomquotes.viewmodel.MainViewModel
 import com.example.android.randomquotes.viewmodel.MainViewModelFactory
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var mainViewModel: MainViewModel
